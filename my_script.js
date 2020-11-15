4d44f76e279a19c2324ff8272f414178fbeac4b1
abdellah-djezzar <74027695+abdellah-djezzar@users.noreply.github.com>
2020-11-15 19:05:51 +0100

    Add files via upload

---
+++ my_script.js
@@ -0,0 +1,28 @@
+﻿document.getElementById("form").addEventListener("submit", function(e){
+
+    var erreur;
+    var nom = document.getElementById("nom");
+    var email = document.getElementById("email");
+    var naissance = document.getElementById("datenaiss");
+    if (!naissance.value) {
+      erreur = "Veuillez renseigner une date de naissance";
+}
+
+if (!email.value) {
+      erreur = "Veuillez renseigner un email";
+}
+
+if (!nom.value) {
+ erreur = "Veuillez renseigner un nom";
+}
+
+
+if (erreur) {
+  e.preventDefault();
+  document.getElementById("erreur").innerHTML = erreur;
+  return false;
+} else {
+  alert('Formulaire envoyé !');
+}
+
+});
